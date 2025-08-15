ccc9e2ccc4d9572ccf93b09fd34f9a73de733850
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2025-08-15 14:06:14 +0100

    added functions for quiz answers to functions file. not yet applied to buttons. Also began adding basic cyber security questions to easy difficulty quiz.

--- src/pages/basicCyberSecurity.jsx
+++ src/pages/basicCyberSecurity.jsx
@@ -6,10 +6,47 @@ const EasyCyberSecurity =()=>{
             <h1>Easy Cyber Security Quiz</h1>
             <h5>Rules:</h5>
             <p>You will be asked 10 questions.</p>
-            <p>You will be given 4 potential answers, with only 1 being correct.</p>
+            <p>You will be given upto 4 potential answers, with only 1 being correct.</p>
             <p>You only get ONE chance to answer.</p>
             <br /><br /><br />
             <p>Here will contain basic cyber security questions. Each question will have 4 options with only 1 correct answer.</p>
+            <div>
+                <h5>Question 1:</h5>
+                <p>What is cyber security?</p>
+                <button>The protection of all things within the digital world, including, but not limited to computers, smartphnes and networks.</button>
+                <button>Protecting a service user's information.</button>
+                <button>The process of testing for weaknesses.</button>
+                <button>Developing a new piece of software for use across the business.</button>
+            </div>
+            <div>
+                <h5>Question 2:</h5>
+                <p>Should you use the same password for everything?</p>
+                <button>Yes. I may forget passwords otherwise.</button>
+                <button>No. I may forget but I can use a password manager.</button>
+            </div>
+            <div>
+                <h5>Question 3:</h5>
+                <p>Why shouldn't you download from untrusted sources?</p>
+                <button>The download may contain malware including viruses or spyware.</button>
+                <button>What do you mean I shouldn't?.</button>
+                <button>Because doing so may prevent a company, such as McAfee from taking money for the same service.</button>
+                <button>There is no such thing as untrusted sources.</button>
+            </div>
+            <div>
+                <h5>Question 4:</h5>
+                <p>In Cyber Security terms, what is a firewall?</p>
+                <button>A literal wall of fire</button>
+                <button>A piece of software to regulate traffic on a network and/or system to aid in protecting against unauthorised access.</button>
+                <button>A method of security designed by Leonardo Da Vinci</button>
+            </div>
+            <div>
+                <h5>Question 5:</h5>
+                <p>What does VPN stand for?</p>
+                <button>Visual Public Nuisance</button>
+                <button>Virtual Portfolio Notification</button>
+                <button>Virtual Private Network</button>
+                <button>Virtual Public Network</button>
+            </div>
         </div>
     )
 }
